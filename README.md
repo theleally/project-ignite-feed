464e3a00e197bef67e0b1fb7c359f7f0d9be396b
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2024-12-18 11:00:13 -0300

    project ignite feed

---
+++ README.md
@@ -0,0 +1,8 @@
+# React + TypeScript + Vite - Projeto Ignite Feed
+
+Esse é um pequeno projeto de um Blog com demonstrações de funcionalidades como:
+
+- Publicação de comentário
+- Listagem de comentários
+- Deletar comentário
+
---
+++ src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { Avatar } from "./Avatar";
+import { Trash, ThumbsUp } from "phosphor-react"
+
+import styles from "./Comment.module.css"
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    // opção - setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1;
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/theleally.png" alt=""/>
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Leandro Allysson</strong>
+              <time title="04 de Dezembro ás 08:43h" dateTime="2024-04-12 08:43:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
